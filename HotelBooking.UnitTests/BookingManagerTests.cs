d88e710f2a38aeabe9a6e95b7ca0b1ef0d7a3523
Martin Grulyó <106872157+mrtng1@users.noreply.github.com>
2025-02-09 23:21:38 +0100

    Basic Tests

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -13,16 +13,24 @@ namespace HotelBooking.UnitTests
         private IBookingManager bookingManager;
         IRepository<Booking> bookingRepository;
 
+        //private DateTime startOccupiedDate;
+        //private DateTime stopOccupiedDate;
+
         public BookingManagerTests(){
+            // fully occupied start & end date
             DateTime start = DateTime.Today.AddDays(10);
             DateTime end = DateTime.Today.AddDays(20);
+            
+            //startOccupiedDate = DateTime.Today.AddDays(10);
+            //stopOccupiedDate = DateTime.Today.AddDays(20);
+            
             bookingRepository = new FakeBookingRepository(start, end);
             IRepository<Room> roomRepository = new FakeRoomRepository();
             bookingManager = new BookingManager(bookingRepository, roomRepository);
         }
 
         [Fact]
-        public async Task FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException()
+        public async Task FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException() // case 1 & 2
         {
             // Arrange
             DateTime date = DateTime.Today;
@@ -35,7 +43,7 @@ namespace HotelBooking.UnitTests
         }
 
         [Fact]
-        public async Task FindAvailableRoom_RoomAvailable_RoomIdNotMinusOne()
+        public async Task FindAvailableRoom_RoomAvailable_RoomIdNotMinusOne() // case 3 & 5
         {
             // Arrange
             DateTime date = DateTime.Today.AddDays(1);
@@ -65,6 +73,58 @@ namespace HotelBooking.UnitTests
             
             // Assert
             Assert.Empty(bookingForReturnedRoomId);
+        }
+        
+        [Fact]
+        public async Task FindAvailableRoom_FullyBooked_RoomIdMinusOne() // case 7
+        {
+            // Arrange
+            DateTime from = DateTime.Today.AddDays(11);
+            DateTime to = DateTime.Today.AddDays(19);
+            
+            // Act
+            int roomId = await bookingManager.FindAvailableRoom(from, to);
+            // Assert
+            Assert.Equal(-1, roomId);
+        }
+        
+        [Fact]
+        public async Task FindAvailableRoom_StartBeforeEndInsideFullyBooked_RoomIdMinusOne() // case 4
+        {
+            // Arrange
+            DateTime from = DateTime.Today.AddDays(9);
+            DateTime to = DateTime.Today.AddDays(19);
+            
+            // Act
+            int roomId = await bookingManager.FindAvailableRoom(from, to);
+            // Assert
+            Assert.Equal(-1, roomId);
+        }
+        
+        [Fact]
+        public async Task FindAvailableRoom_StartInsideEndAfterFullyBooked_RoomIdMinusOne() // case 8
+        {
+            // Arrange
+            DateTime from = DateTime.Today.AddDays(11);
+            DateTime to = DateTime.Today.AddDays(21);
+            
+            // Act
+            int roomId = await bookingManager.FindAvailableRoom(from, to);
+            // Assert
+            Assert.Equal(-1, roomId);
+        }
+        
+        [Fact]
+        public async Task FindAvailableRoom_InsideFullyBooked_RoomIdMinusOne() // case 6
+        {
+            // Arrange
+            DateTime from = DateTime.Today.AddDays(9);
+            DateTime to = DateTime.Today.AddDays(21);
+            
+            // Act
+            int roomId = await bookingManager.FindAvailableRoom(from, to);
+            // Assert
+            Assert.Equal(-1, roomId);
         }
 
     }
